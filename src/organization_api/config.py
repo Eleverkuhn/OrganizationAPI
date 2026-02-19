d4caf460b20581a2f755aba1e102434403f66549
Eleverkuhn <Eleverkuhn@users.noreply.github.com>
2026-02-19 18:59:50 +0300

    feat(project-setup): Add alembic. Add SQLAlchemy engine

---
+++ src/organization_api/config.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent.parent
+ENV = BASE_DIR / ".env"
+
+
+class Env(BaseSettings):
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_host: str
+    postgres_port: str
+
+    postgres_test_db: str
+    postgres_test_port: str
+
+    fastapi_host: str
+    fastapi_port: str
+
+    pythonpath: str
+
+    model_config = SettingsConfigDict(env_file=ENV)
+
+
+env = Env()
---
+++ src/organization_api/data/db_connection.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from config import env
+
+DB_URL = (
+    f"postgresql+asyncpg://{env.postgres_user}:"
+    f"{env.postgres_password}"
+    f"@{env.postgres_host}:{env.postgres_port}/{env.postgres_db}"
+)
+
+DB_SYNC_URL = (
+    f"postgresql+psycopg://{env.postgres_user}:"
+    f"{env.postgres_password}"
+    f"@{env.postgres_host}:{env.postgres_port}/{env.postgres_db}"
+)
+
+engine = create_async_engine(DB_URL, echo=True)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
