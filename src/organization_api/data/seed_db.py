b8e9f4de9f8e743456c43d5a28bf31908f600458
Eleverkuhn <Eleverkuhn@users.noreply.github.com>
2026-02-20 13:05:47 +0300

    feat(seed-db): Add script functionality to seed_db.py. Add comments for a reviewr

--- src/organization_api/data/seed_db.py
+++ src/organization_api/data/seed_db.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 from datetime import datetime, date
 from pathlib import Path
@@ -8,12 +9,25 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from config import BASE_DIR
 from data.repositories import BaseRepository
 from data.sql_models import Department, Employee
+from data.db_connection import get_async_session
 
 EmployeeData: TypeAlias = dict[str, int | str | date | None]
 
 FIXTURE_DIR = BASE_DIR / "src" / "organization_api" / "data" / "fixtures"
 
 FIXTURE_TO_MODEL = {"departments.json": Department, "employees.json": Employee}
+
+
+async def main() -> None:
+    session = await anext(get_async_session())
+    # Имитация работы контекстного менеджера, т.к. я не могу обернуть get_async_session в декоратор из-за
+    # @contextlib.contextmanager из-за того, что нужно будет использовать эту функцию как депенденси
+    try:
+        await seed_db(session)
+    except:
+        raise
+    finally:
+        await session.close()
 
 
 async def seed_db(session: AsyncSession) -> None:
@@ -30,6 +44,7 @@ def get_data_from_fixture(fixture_name: str) -> list[dict]:
 
 
 def read_fixture(fixture: Path) -> list[dict]:
+    # Маленький трейдоф. Лучше блокирующая функция, чем создавать ещё один синхронный движок для БД из-за одного скрипта
     with fixture.open("r", encoding="utf-8") as file:
         data = json.load(file)
     return data
@@ -43,3 +58,7 @@ def check_date_fields(data: Sequence[Mapping]) -> None:
 
 def convert_str_to_date(entry: EmployeeData) -> None:
     entry["hired_at"] = datetime.strptime(entry["hired_at"], "%Y-%m-%d").date()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
