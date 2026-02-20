a7b8b7fe9386d81cffb7c68bc727c737de71ad45
Eleverkuhn <Eleverkuhn@users.noreply.github.com>
2026-02-20 13:10:10 +0300

    feat(seed-db): Add function for session creation

--- src/organization_api/data/db_connection.py
+++ src/organization_api/data/db_connection.py
@@ -1,4 +1,10 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+)
 
 from config import env
 
@@ -8,6 +14,10 @@ DB_URL = (
     f"@{env.postgres_host}:{env.postgres_port}/{env.postgres_db}"
 )
 
-
 engine = create_async_engine(DB_URL, echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
