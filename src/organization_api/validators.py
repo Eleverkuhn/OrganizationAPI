537cd606d0bd1bb25ebe07aec88e70ef8a5b8c5e
Eleverkuhn <Eleverkuhn@users.noreply.github.com>
2026-02-21 13:04:38 +0300

    feat(api): Add check whether provided parent_id is equal to a newly created department

--- src/organization_api/validators.py
+++ src/organization_api/validators.py
@@ -1,12 +1,15 @@
 from models import DepartmentIn
 from data.repositories import DepartmentRepository
 
+# TODO:: Rework this functions into a single class
+
 
 async def validate_department_creation_data(
     repository: DepartmentRepository, data: DepartmentIn
 ) -> None:
     if data.parent_id:
         await check_department_name_is_unique(repository, data)
+        await check_parent_id_eq_new_department_id(repository, data)
 
 
 async def check_department_name_is_unique(
@@ -14,4 +17,19 @@ async def check_department_name_is_unique(
 ) -> None:
     parent_department = await repository.get_with_children(data.parent_id)
     if data.name in (dep.name for dep in parent_department.children):
+        raise ValueError
+
+
+async def check_parent_id_eq_new_department_id(
+    repository: DepartmentRepository, data: DepartmentIn
+) -> None:
+    departments = await repository.get_all()
+
+    if departments:
+        last_department = departments[len(departments) - 1]
+        new_department_id = last_department.id + 1
+    else:
+        new_department_id = 1
+
+    if data.parent_id == new_department_id:
         raise ValueError
--- tests/test_integration/test_validators.py
+++ tests/test_integration/test_validators.py
@@ -1,7 +1,10 @@
 import pytest
 
 from models import DepartmentIn
-from validators import check_department_name_is_unique
+from validators import (
+    check_department_name_is_unique,
+    check_parent_id_eq_new_department_id,
+)
 from data.repositories import DepartmentRepository
 from tests.conftest import DepartmentData
 
@@ -17,3 +20,13 @@ async def test_check_name_is_unique_raises_value_error(
     data = DepartmentIn(**existing_department)
     with pytest.raises(ValueError):
         await check_department_name_is_unique(department_repository, data)
+
+
+@pytest.mark.asyncio
+async def test_check_parent_id_eq_new_department_id(
+    department_repository: DepartmentRepository,
+) -> None:
+    data = {"name": "test_dep", "parent_id": 1}
+    data = DepartmentIn(**data)
+    with pytest.raises(ValueError):
+        await check_parent_id_eq_new_department_id(department_repository, data)
