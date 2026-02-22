12d5b150587f87ff40232f932d083c4c0ebf968b
Eleverkuhn <Eleverkuhn@users.noreply.github.com>
2026-02-22 19:03:00 +0300

    refactor(api): Incapsulate getting of next id for department table to DepartmentRepository

--- src/organization_api/validators.py
+++ src/organization_api/validators.py
@@ -1,9 +1,15 @@
 from loguru import logger
 
-from models import DepartmentIn
+from models import DepartmentIn, DepartmentChange
 from data.repositories import DepartmentRepository
 
-# TODO:: Rework this functions into a single class
+# TODO: Rework this functions into a single class
+
+
+async def validate_department_change_data(
+    repository: DepartmentRepository, data: DepartmentChange
+) -> None:
+    pass
 
 
 async def validate_department_creation_data(
@@ -20,21 +26,17 @@ async def check_department_name_is_unique(
     parent_department = await repository.get_with_children(data.parent_id)
     if data.name in (dep.name for dep in parent_department.children):
         raise ValueError(
-            "Department-child name should be unqie for a single department-parent"
+            "Department-child name should be unique for a single department-parent"
         )
 
 
 async def check_parent_id_eq_new_department_id(
     repository: DepartmentRepository, data: DepartmentIn
 ) -> None:
-    departments = await repository.get_all()
+    new_department_id = await repository._get_new_department_id()
+    check_ids_are_eq(new_department_id, data.parent_id)
 
-    if departments:
-        last_department = departments[len(departments) - 1]
-        new_department_id = last_department.id + 1
-    else:
-        new_department_id = 1
 
-    # TODO: the code above should be moved into a dinstinct function
-    if data.parent_id == new_department_id:
+def check_ids_are_eq(first: int, second: int) -> None:
+    if first == second:
         raise ValueError("Department cannot be a parent to itself")
